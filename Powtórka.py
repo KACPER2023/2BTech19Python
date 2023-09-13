89d6944de0736b5df3bb967fdcd08e99fd31346c
KACPER2023 <114583644+KACPER2023@users.noreply.github.com>
2023-09-13 12:14:20 +0000

    test

---
+++ Powtórka.py
@@ -0,0 +1,20 @@
+# Zad 1
+# a = int(input())
+# b = int(input())
+# print((a**2)+(b**2))
+
+# Zad 2
+# a = int(input())
+# b = int(input())
+# print((a+b)**2)
+
+# Zad 3
+# a = int(input())
+# b = int(input())
+# print((a - b)**3)
+
+# Zad 4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a*b*c)
